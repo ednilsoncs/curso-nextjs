5dfccdf9a58766513627767916417801b432e23b
ednilsoncs <ednilsonsantos43@gmail.com>
2021-02-20 22:33:04 -0300

    chore(styles): configure styled componente to work with this project

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+  }
+
+  body{
+    background: #121214;
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+`
