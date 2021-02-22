482a2fee58d16889dfb62cce8c93b53c4f307233
ednilsoncs <ednilsonsantos43@gmail.com>
2021-02-22 08:53:13 -0300

    chore(project): indente document file and added home initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react'
 import { Title } from '../styles/pages/Home'
+interface IProduct {
+  id:string;
+  title: string;
+}
 
 export default function Home() {
+
+  const [recommendedProducts, setRecommendedProducts]= useState<IProduct[]>([])
+  
+  useEffect(()=>{
+       fetch('http://localhost:3333/recommended').then(response =>{
+         response.json().then(data=>{
+           setRecommendedProducts(data)
+         })
+       })
+  },[])
+
   return (
     <div >
-      <Title>Hello word</Title>
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(recommendedProduct=>{
+            return(
+              <li key={recommendedProduct.id}>
+                   {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
     </div>
 
   )
