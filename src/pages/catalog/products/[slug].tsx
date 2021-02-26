86acc54f1edc8ff3938bddaa44d795ef70db9533
ednilsoncs <ednilsonsantos43@gmail.com>
2021-02-25 23:28:58 -0300

    chore(variaveis): adicionado variaveis de ambiente

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,10 +1,25 @@
 import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import { useState } from 'react';
+
+const  AddToCartModal = dynamic(()=> import('../../../components/AddToCartModal'),{
+  loading: ()=> <p>Loading...</p>
+})
+
 export default function Product(){
+  const  [isAddToCartModalVisible, setAddToCartModalVisible] =  useState(false)
 
   const  router = useRouter();
-
+  
+  const  handleAddToCart = () =>{
+    setAddToCartModalVisible(true)
+  }
 
   return (
-    <h1>{router.query.slug}</h1>
+    <div>
+      <h1>{router.query.slug}</h1>
+      <button onClick={handleAddToCart}> Add to cart</button>
+      { isAddToCartModalVisible && <AddToCartModal/>}
+    </div>
   )
 }
